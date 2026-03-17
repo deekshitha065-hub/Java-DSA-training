a5b08b49bacb9dfd44b477dd2f2b34a9d4a1308b
deekshitha065-hub <deekshitha065@gmail.com>
2026-03-17 16:00:39 +0530

    added all the codes till D11-S11

---
+++ src/com/advanced/dsa/dataStructure/linear/stack/PostFixEvaluation.java
@@ -0,0 +1,40 @@
+package com.advanced.dsa.dataStructure.linear.stack;
+
+import java.util.Stack;
+
+public class PostFixEvaluation {
+    public static void main(String[] args) {
+        String s1="23*54*+9-";
+        System.out.println(evaluate(s1));
+        String s2="45+23*+";
+        System.out.println(evaluate(s2));
+    }
+    public static int evaluate(String exp){
+        Stack<Integer> stack= new Stack<>();
+        for (char c:exp.toCharArray()){
+            if(Character.isDigit(c)){
+                stack.push(c-'0');//Store the integer value
+            }else{
+                int b=stack.pop();
+                int a=stack.pop();
+                switch(c){
+                    case '+'->
+                        stack.push(a+b);
+
+                    case '-'->
+                        stack.push(a-b);
+
+                    case '*'->
+                        stack.push(a*b);
+
+                    case '/'->
+                        stack.push(a/b);
+
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+
+}
