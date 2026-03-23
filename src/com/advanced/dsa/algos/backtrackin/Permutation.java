ff3304925519d4e1c6a3ca2eb21bc563e35963ba
deekshitha065-hub <deekshitha065@gmail.com>
2026-03-23 16:38:03 +0530

    added all the code till today

---
+++ src/com/advanced/dsa/algos/backtrackin/Permutation.java
@@ -0,0 +1,29 @@
+package com.advanced.dsa.algos.backtrackin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        List<List<Integer>> result=new ArrayList<>();
+        boolean[] used = new boolean[arr.length];
+        backtrack(arr,result,new ArrayList<>(),used);
+        System.out.println(result);
+    }
+    static void backtrack(int[] nums, List<List<Integer>> result, List<Integer > temp, boolean[] used){
+        if (temp.size()==nums.length){
+            result.add(new ArrayList<>(temp));
+        }
+        for(int i=0; i<nums.length; i++){
+            if (used[i] )continue;
+            temp.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, result, temp, used);
+            temp.remove(temp.size()-1);
+            used[i]=false;
+
+            }
+        }
+    }
+
